96f2dcfc72bb5526097c8fdc2de39f82f9601bd9
SadmanTariq <sadmantariq@gmail.com>
2020-12-22 23:22:27 +0600

    Ship rotates

--- godot/entities/ships/player/player.gd
+++ godot/entities/ships/player/player.gd
@@ -1,11 +1,32 @@
-extends Spatial
+extends KinematicBody
 
-export(NodePath) var camera_path
+export var max_rotation_speed = 180  # Degrees per second
+export var max_bank_angle = 60  # Degrees
+export(float, 0, 3) var rotation_factor: float
 
-func _ready():
-	var camera = get_node(camera_path)
-	$CameraAnchor.remote_path = $CameraAnchor.call("get_path_to", camera)
 
 func _physics_process(delta):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		translate(Vector3(0, 0, -10 * delta))
+	_rotate(delta)
+	
+
+func _rotate(delta):
+	var forward: Vector3 = -$Ship.transform.basis.z
+	
+	var angle_to_mouse = forward.angle_to(_get_rotate_target())
+	angle_to_mouse *= sign(forward.cross(_get_rotate_target()).y)
+	
+	var amount = pow(sin(abs(angle_to_mouse) / 2), rotation_factor)
+	amount *= sign(angle_to_mouse)
+	
+	var rotate_angle = amount * deg2rad(max_rotation_speed) * delta
+	
+	$Ship.rotate_y(rotate_angle)
+	$Ship.rotation.z = amount * deg2rad(max_bank_angle)
+
+func _get_rotate_target() -> Vector3:
+	var mousepos = get_viewport().get_mouse_position()
+	var camera = get_viewport().get_camera()
+	var rayorigin = camera.project_ray_origin(mousepos)
+	var mouseray = camera.project_ray_normal(mousepos)
+	var pos = rayorigin + (-rayorigin.y / mouseray.y) * mouseray
+	return pos
