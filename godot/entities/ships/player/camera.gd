b576f65e29daadfb8f8e98e3003c9da5e2e45954
SadmanTariq <sadmantariq@gmail.com>
2020-12-23 00:15:28 +0600

    Acceleration done

--- godot/entities/ships/player/camera.gd
+++ godot/entities/ships/player/camera.gd
@@ -1,9 +1,22 @@
 extends Spatial
 
+
 export var zoom_level = 0.5 setget _set_zoom_level
 export var zoom_increment = 0.02
 export var min_distance = 20
 export var max_distance = 100
+
+
+func get_world_global_mouse_pos() -> Vector3:
+	var mousepos = get_viewport().get_mouse_position()
+	var rayorigin = $Camera.project_ray_origin(mousepos)
+	var mouseray = $Camera.project_ray_normal(mousepos)
+	var pos = rayorigin + (-rayorigin.y / mouseray.y) * mouseray
+	return pos + global_transform.origin
+
+
+func _ready():
+	Globals.camera = self
 
 func _input(event):
 	if event is InputEventMouseButton:
--- godot/entities/ships/player/player.gd
+++ godot/entities/ships/player/player.gd
@@ -1,16 +1,29 @@
-extends KinematicBody
+extends RigidBody
 
 export var max_rotation_speed = 180  # Degrees per second
 export var max_bank_angle = 60  # Degrees
 export(float, 0, 3) var rotation_factor: float
+export var thrust = 150.0
+#export var damping = 2
 
 
 func _physics_process(delta):
+#	translation.y = 0
+#	linear_velocity.y = 0
 	_rotate(delta)
 	
+	if Input.is_action_pressed("accelerate"):
+		_accelerate(delta)
+	
+#	print(global_transform.origin)
+
+func _accelerate(delta):
+	var resultant = -$Ship.transform.basis.z * thrust * delta
+	resultant.y = 0
+	apply_central_impulse(resultant)
 
 func _rotate(delta):
-	var forward: Vector3 = -transform.basis.z
+	var forward: Vector3 = -$Ship.transform.basis.z
 	
 	var angle_to_mouse = forward.angle_to(_get_rotate_target())
 	angle_to_mouse *= sign(forward.cross(_get_rotate_target()).y)
@@ -20,13 +33,16 @@ func _rotate(delta):
 	
 	var rotate_angle = amount * deg2rad(max_rotation_speed) * delta
 	
-	rotate_y(rotate_angle)
-	rotation.z = amount * deg2rad(max_bank_angle)
+	$Ship.rotate_y(rotate_angle)
+	$Ship.rotation.z = amount * deg2rad(max_bank_angle)
 
 func _get_rotate_target() -> Vector3:
-	var mousepos = get_viewport().get_mouse_position()
-	var camera = get_viewport().get_camera()
-	var rayorigin = camera.project_ray_origin(mousepos)
-	var mouseray = camera.project_ray_normal(mousepos)
-	var pos = rayorigin + (-rayorigin.y / mouseray.y) * mouseray
-	return pos
+	if Globals.camera == null:
+		return Vector3()
+	return Globals.camera.get_world_global_mouse_pos() - global_transform.origin
+#	var mousepos = get_viewport().get_mouse_position()
+#	var camera = get_viewport().get_camera()
+#	var rayorigin = camera.project_ray_origin(mousepos)
+#	var mouseray = camera.project_ray_normal(mousepos)
+#	var pos = rayorigin + (-rayorigin.y / mouseray.y) * mouseray
+#	return pos
